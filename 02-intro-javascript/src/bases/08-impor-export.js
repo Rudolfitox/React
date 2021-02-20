ef088a9c57f5113c3cb1d8ceb19faa052ec5f4c7
Rudolf <rudolfgarcia94@gmail.com>
2021-02-20 15:43:27 -0600

    uso de fetch

--- 02-intro-javascript/src/bases/08-impor-export.js
+++ 02-intro-javascript/src/bases/08-impor-export.js
@@ -1,10 +1,10 @@
 //IMPORT EXPORT
 import heroes,{owners} from '../data/heroes';
 
-console.log(owners)
+//console.log(owners)
 
-const getHeroeById = (id) => heroes.find(h => h.id == id);
-console.log(getHeroeById(2))
+export const getHeroeById = (id) => heroes.find(h => h.id === id);
+//console.log(getHeroeById(2))
 
-const getHeroeByOwner = (owner) => heroes.filter(h => h.owner == owner);
-console.log(getHeroeByOwner('DC'));
+const getHeroeByOwner = (owner) => heroes.filter(h => h.owner === owner);
+//console.log(getHeroeByOwner('DC'));
--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -1,10 +1,52 @@
-//IMPORT EXPORT
-import heroes,{owners} from './data/heroes';
+import { getHeroeById } from "./bases/08-impor-export";
 
-console.log(owners)
+//PROMESAS
+// const promesa = new Promise((resolve,reject) => {
+//     setTimeout(() => {
+//         //console.log('Dos segundos despues');
+    
+//        const heroe = getHeroeById(2);
+//        resolve(heroe);
+//        //reject('No se pudo encontrar el héroe');
+//        console.log(heroe);
+//     },2000)
+// });
 
-const getHeroeById = (id) => heroes.find(h => h.id == id);
-console.log(getHeroeById(2))
+// promesa.then((heroe) => {
+//     console.log('heroe',heroe);
+// }).catch(err => console.warn(err));
 
-const getHeroeByOwner = (owner) => heroes.filter(h => h.owner == owner);
-console.log(getHeroeByOwner('DC'));
+// const promesa = new Promise((resolve,reject) => {
+//     setTimeout(() => {
+//         //console.log('Dos segundos despues');
+    
+//        const heroe = getHeroeById(2);
+//        resolve(heroe);
+//        //reject('No se pudo encontrar el héroe');
+//        console.log(heroe);
+//     },2000)
+// });
+
+
+const getHeroeByIdAsyync = (id) => {
+    
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {    
+            const heroe = getHeroeById(id);
+            if(heroe){
+                resolve(heroe);
+        
+            }else{
+                reject('No se pudo encontrar el héroe');
+            }
+        },2000)
+    });
+}
+
+// getHeroeByIdAsyync(1)
+// .then(heroe => console.log('Heroe',heroe))
+// .catch(err => console.warn(err));
+
+getHeroeByIdAsyync(1)
+.then(console.log)
+.catch(console.warn)
