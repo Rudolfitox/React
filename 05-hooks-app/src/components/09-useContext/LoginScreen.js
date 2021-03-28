3507d08165526e94a07c88f84551f34546951e34
Rodolfo Fabian <rudolfgarcia94@gmail.com>
2021-03-28 11:46:13 -0600

    more changes

--- 05-hooks-app/src/components/09-useContext/LoginScreen.js
+++ 05-hooks-app/src/components/09-useContext/LoginScreen.js
@@ -1,19 +1,16 @@
-import React,{useContext} from 'react';
-import { UserContext } from './UserContext';
+import React from 'react'
 
 export const LoginScreen = () => {
     //1. Obtener la referencia al userContext
     //2. setUser
 
-    const {setUser} = useContext(UserContext);
-    
     return (
         <div>
             <h1>LoginScreen</h1>
             <hr/>
             <button
                 className="btn btn-primary"
-                onClick={()=>{setUser({id:12345,name:'Rudolf'})}}
+                onClick={()=>{}}
                 >
                 Login
             </button>
